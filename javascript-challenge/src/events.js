f7345027813562b85cf336d4586c053824820ed3
Matthew <mleyburn01@qub.ac.uk>
2021-03-01 18:22:59 +0000

    completed javascript challenges

--- javascript-challenge/src/events.js
+++ javascript-challenge/src/events.js
@@ -1,3 +1,6 @@
+const { parse, differenceInDays, addDays } = require('date-fns');
+
+const DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 /** 
   An event could look like this:
   ```
@@ -30,9 +33,69 @@
 
  Your solution should not modify any of the function arguments
 */
-const groupEventsByDay = (events) => {
-  return events;
+const events = [
+  {
+    id: 107,
+    startsAt: '2021-01-27T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
+    title: 'Daily walk',
+  },
+  {
+    id: 101,
+    startsAt: '2021-01-25T13:01:11Z',
+    endsAt: '2021-01-25T15:01:11Z',
+    title: 'Daily walk',
+  },
+  {
+    id: 162,
+    startsAt: '2021-01-28T13:01:11Z',
+    endsAt: '2021-01-28T15:01:11Z',
+    title: 'Dinner',
+  },
+  {
+    id: 184,
+    startsAt: '2021-01-27T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
+    title: 'Dinner',
+  },
+  {
+    id: 117,
+    startsAt: '2021-01-28T13:01:11Z',
+    endsAt: '2021-01-28T15:01:11Z',
+    title: 'Daily walk',
+  },
+];
+
+const parseStringToDate = (date) => parse(date, DATE_FORMAT, new Date());
+
+const findDifferenceInDays = (dayA, dayB) =>
+  differenceInDays(
+    parseStringToDate(dayA.startsAt),
+    parseStringToDate(dayB.startsAt),
+  );
+
+const sortEventsByStartDate = (events) =>
+  events.sort((dateA, dateB) => findDifferenceInDays(dateA, dateB));
+
+const groupByStartDate = (events) => {
+  const firstStartDate = events[0];
+
+  return events.reduce((acc, event) => {
+    let daysToEvent = findDifferenceInDays(event, firstStartDate);
+    acc[daysToEvent] = acc[daysToEvent] || [];
+    acc[daysToEvent].push(event);
+    return acc;
+  }, {});
 };
+
+const groupEventsByDay = (events) => {
+  const sortedStartDates = sortEventsByStartDate(events);
+  const groupedEventsByDay = groupByStartDate(sortedStartDates);
+
+  return groupedEventsByDay;
+};
+
+// groupEventsByDay(events);
 
 /** 
   Adjust the start and end date of an event so it maintains its total duration, but is moved `toDay`.
@@ -69,6 +132,49 @@ const groupEventsByDay = (events) => {
 
   Your solution should not modify any of the function arguments
 */
+const changeEventDate = (eventToBeUpdated, newDay) => {
+  const newEventStartDate = addDays(
+    parseStringToDate(eventToBeUpdated.startsAt),
+    newDay,
+  );
+  const newEventEndDate = addDays(
+    parseStringToDate(eventToBeUpdated.endsAt),
+    newDay,
+  );
+  return (changedEvent = {
+    ...eventToBeUpdated,
+    startsAt: newEventStartDate.toISOString(),
+    endsAt: newEventEndDate.toISOString(),
+  });
+};
+
 const moveEventToDay = (eventsByDay, id, toDay) => {
+  if (toDay < 0)
+    throw new Error(
+      `A toDay value of ${toDay} was given. This value should be 0 or greater`,
+    );
+  const copyEventsByDay = { ...eventsByDay };
+
+  Object.keys(copyEventsByDay).map((key) => {
+    copyEventsByDay[key].map((event, index) => {
+      if (event.id === id) {
+        const newDay = toDay - Number(key);
+        const updatedEvent = changeEventDate(event, newDay);
+        copyEventsByDay[toDay] = copyEventsByDay[toDay] || [];
+        copyEventsByDay[toDay].push(updatedEvent);
+        copyEventsByDay[key].splice(index, 1);
+      }
+    });
+  });
+
+  if (JSON.stringify(copyEventsByDay) === JSON.stringify(eventsByDay))
+    throw new Error(`Did not find event id of ${id}`);
+  else eventsByDay = { ...copyEventsByDay };
+
   return eventsByDay;
 };
+
+// const eventsByDay = groupEventsByDay(events);
+// moveEventToDay(eventsByDay, 107, 6);
+
+module.exports = { groupEventsByDay, moveEventToDay };
--- javascript-challenge/src/events.test.js
+++ javascript-challenge/src/events.test.js
@@ -1,3 +1,186 @@
-test('groupEventsByDay', () => {});
+const events = require('./events');
 
-test('moveEvent', () => {});
+describe('groupEventsByDay', () => {
+  const testEvents = [
+    {
+      id: 107,
+      startsAt: '2021-01-27T13:01:11Z',
+      endsAt: '2021-01-27T15:01:11Z',
+      title: 'Daily walk',
+    },
+    {
+      id: 101,
+      startsAt: '2021-01-25T13:01:11Z',
+      endsAt: '2021-01-25T15:01:11Z',
+      title: 'Daily walk',
+    },
+    {
+      id: 162,
+      startsAt: '2021-01-28T13:01:11Z',
+      endsAt: '2021-01-28T15:01:11Z',
+      title: 'Dinner',
+    },
+    {
+      id: 184,
+      startsAt: '2021-01-27T13:01:11Z',
+      endsAt: '2021-01-27T15:01:11Z',
+      title: 'Dinner',
+    },
+  ];
+
+  const expectedResult = {
+    0: [
+      {
+        id: 101,
+        startsAt: '2021-01-25T13:01:11Z',
+        endsAt: '2021-01-25T15:01:11Z',
+        title: 'Daily walk',
+      },
+    ],
+    2: [
+      {
+        id: 107,
+        startsAt: '2021-01-27T13:01:11Z',
+        endsAt: '2021-01-27T15:01:11Z',
+        title: 'Daily walk',
+      },
+      {
+        id: 184,
+        startsAt: '2021-01-27T13:01:11Z',
+        endsAt: '2021-01-27T15:01:11Z',
+        title: 'Dinner',
+      },
+    ],
+    3: [
+      {
+        id: 162,
+        startsAt: '2021-01-28T13:01:11Z',
+        endsAt: '2021-01-28T15:01:11Z',
+        title: 'Dinner',
+      },
+    ],
+  };
+
+  test('Sort each event by day and group events by day', () => {
+    const eventsByDay = events.groupEventsByDay(testEvents);
+    expect(eventsByDay).toEqual(expectedResult);
+  });
+
+  const singleEvent = [
+    {
+      id: 107,
+      startsAt: '2021-01-27T13:01:11Z',
+      endsAt: '2021-01-27T15:01:11Z',
+      title: 'Daily walk',
+    },
+  ];
+
+  const expectedSingleEventResult = {
+    0: [
+      {
+        id: 107,
+        startsAt: '2021-01-27T13:01:11Z',
+        endsAt: '2021-01-27T15:01:11Z',
+        title: 'Daily walk',
+      },
+    ],
+  };
+
+  test('Returns a single JSON if single event is given', () => {
+    const eventsByDay = events.groupEventsByDay(singleEvent);
+    expect(eventsByDay).toEqual(expectedSingleEventResult);
+  });
+
+  const emptyEventList = [];
+
+  test('Returns empty JSON if events list is empty', () => {
+    const eventsByDay = events.groupEventsByDay(emptyEventList);
+    expect(eventsByDay).toEqual({});
+  });
+});
+
+describe('moveEvent', () => {
+  const testEvents = [
+    {
+      id: 107,
+      startsAt: '2021-01-27T13:01:11Z',
+      endsAt: '2021-01-27T15:01:11Z',
+      title: 'Daily walk',
+    },
+    {
+      id: 101,
+      startsAt: '2021-01-25T13:01:11Z',
+      endsAt: '2021-01-25T15:01:11Z',
+      title: 'Daily walk',
+    },
+    {
+      id: 162,
+      startsAt: '2021-01-28T13:01:11Z',
+      endsAt: '2021-01-28T15:01:11Z',
+      title: 'Dinner',
+    },
+    {
+      id: 184,
+      startsAt: '2021-01-27T13:01:11Z',
+      endsAt: '2021-01-27T15:01:11Z',
+      title: 'Dinner',
+    },
+  ];
+
+  const expectedMoveEventResult = {
+    0: [
+      {
+        id: 101,
+        startsAt: '2021-01-25T13:01:11Z',
+        endsAt: '2021-01-25T15:01:11Z',
+        title: 'Daily walk',
+      },
+    ],
+    2: [
+      {
+        id: 184,
+        startsAt: '2021-01-27T13:01:11Z',
+        endsAt: '2021-01-27T15:01:11Z',
+        title: 'Dinner',
+      },
+    ],
+    3: [
+      {
+        id: 162,
+        startsAt: '2021-01-28T13:01:11Z',
+        endsAt: '2021-01-28T15:01:11Z',
+        title: 'Dinner',
+      },
+    ],
+    6: [
+      {
+        id: 107,
+        startsAt: '2021-01-31T13:01:11.000Z',
+        endsAt: '2021-01-31T15:01:11.000Z',
+        title: 'Daily walk',
+      },
+    ],
+  };
+
+  test('Returns event list after event is moved forward', () => {
+    const eventsByDay = events.groupEventsByDay(testEvents);
+    const updatedEventsByDay = events.moveEventToDay(eventsByDay, 107, 6);
+    expect(updatedEventsByDay).toEqual(expectedMoveEventResult);
+  });
+
+  test('Returns an error after a negative toDay value is given', () => {
+    const toDay = -3;
+    const eventsByDay = events.groupEventsByDay(testEvents);
+    expect(() => events.moveEventToDay(eventsByDay, 107, toDay)).toThrow(
+      `A toDay value of ${toDay} was given. This value should be 0 or greater`,
+    );
+  });
+
+  test('Returns an error after a an invalid id id given', () => {
+    const id = 1073;
+    const eventsByDay = events.groupEventsByDay(testEvents);
+    expect(() => events.moveEventToDay(eventsByDay, id, 3)).toThrow(
+      `Did not find event id of ${id}`,
+    );
+  });
+});
